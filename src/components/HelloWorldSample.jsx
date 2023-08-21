dce8e87e1e69d71f63745297d7ccc861394cc9d4
Ammarhr <hariry49@gmail.com>
2023-08-21 08:51:14 +0000

    add list attribute to the webcomponent optionallys

--- src/components/HelloWorldSample.jsx
+++ src/components/HelloWorldSample.jsx
@@ -1,4 +1,4 @@
-import { createElement, useEffect } from "react";
+import { createElement, useEffect, useState } from "react";
 
 export function HelloWorldSample({
     webComponentsAttributes,
@@ -8,17 +8,18 @@ export function HelloWorldSample({
     listAttribute2,
     ListName
 }) {
+    const [objectList, setObjectList] = useState([]);
     useEffect(() => {
         if (webComponentsList) {
             if (webComponentsList.status === "available") {
-                console.info(webComponentsList, "webComponentsList", ListName, "ListName");
                 const sortInstrs = [[listAttribute1.id, "asc"]];
-                webComponentsList.setSortOrder(sortInstrs);
+                // webComponentsList.setSortOrder(sortInstrs);
                 const attributeListValues = webComponentsList.items.map(item => {
+                    console.info(item, "item");
                     const dateValue = { list1: listAttribute1.get(item).value, list2: listAttribute2.get(item).value };
                     return dateValue;
                 });
-                console.info(attributeListValues, "attributeListValues");
+                setObjectList(attributeListValues);
             }
         }
     }, [webComponentsAttributes, webComponentTag, webComponentsList, webComponentsList?.status]);
@@ -27,14 +28,16 @@ export function HelloWorldSample({
             const attributes = {};
             const attributesProps = webComponentsAttributes;
             for (let index = 0; index < attributesProps.length; index++) {
-                console.info(attributesProps[index], "attributesProps[index]");
                 const attr = attributesProps[index];
                 attributes[attr.atrributeName] = attr.attributeValue.displayValue;
             }
+            if (objectList && objectList.length) {
+                attributes["list"] = JSON.stringify(objectList);
+            }
+
             let Component = createElement(webComponentTag, {
                 ...attributes
             });
-            // console.info(attributes, "attributes");
             return Component;
         } else {
             return createElement("div", null);
